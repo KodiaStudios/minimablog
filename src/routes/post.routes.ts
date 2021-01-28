8dbcc0a987d4d2597a98118b2b165afa2a4efd3b
Pontus N <pontusnorr@gmail.com>
2021-01-28 12:55:41 +0100

    changed stub

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -7,7 +7,7 @@ router.get("/", async (req: any, res: any) => {
     try {
         posts = await Post.find().populate("author");
         for (const post of posts) {
-            post.stub = post.title.replace(" ", "-").replace("'", "") + "." + post._id;
+            post.stub = post.title.replace(" ", "-").replace("'", "") + "-" + post._id;
         }
     } catch (err) {
         console.log(err);
@@ -18,12 +18,12 @@ router.get("/", async (req: any, res: any) => {
 
 router.get("/:stub", async (req: any, res: any) => {
     let valid = true;
-    const stubs = req.params.stub.split(".");
+    const stubs = req.params.stub.split("-");
     const stub = stubs[stubs.length - 1];
     let post: any = null;
     try {
         post = await Post.findOne({_id: stub}).populate("author");
-        post.stub = post.title.replace(" ", "-").replace("'", "") + "." + post._id;
+        post.stub = post.title.replace(" ", "-").replace("'", "") + "-" + post._id;
     } catch (err) {
         console.log(err);
         post = {};
